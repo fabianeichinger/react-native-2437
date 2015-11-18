cb4d240b8edcf2a78d6f9d225e57ede997ded302
Fabian Eichinger <fabian.eichinger@gmail.com>
2015-11-18 01:21:34 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Reproduction of React Native issue \#2437
+
+GIFs are from [here](http://surrogateself.tumblr.com/).
+
+Will crash after a few seconds on iPhone 5s / iOS 9.1 simulators.
---
+++ index.ios.js
@@ -0,0 +1,47 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+  StyleSheet,
+  Image,
+  Text,
+  View,
+} = React;
+
+class BadAccess extends React.Component {
+
+  componentWillMount() {
+    let interval = setInterval(() => this.setState(state => ({i: state.i+1})), 59);
+    this.setState({interval, i: 0});
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.interval);
+  }
+
+  render() {
+    let {i} = this.state;
+    let insetHeight = (i % 10) * 10;
+
+    let gifUri = ((i % 5) + 1) + '.gif';
+
+    return (
+      <View style={styles.container}>
+        <Image style={{flex: 1}} source={{uri: gifUri}} />
+      </View>
+    );
+  }
+};
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+AppRegistry.registerComponent('BadAccess', () => BadAccess);
